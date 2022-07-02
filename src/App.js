8acc885ea3fc062d1d9222c3aa3e3085e36a9df6
Bruno Peres <bruno_16515@hotmail.com>
2022-07-02 11:08:53 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+// components
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App
